78edf1d4c86d6cc8382413c1783dfd3d6a6b9450
Umut Arpat <arptumut@gmail.com>
2020-08-06 21:05:31 +0300

    added headline. removed html,body tag from global scss.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroComponent } from './shared/components/hero/hero.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/anasayfa', pathMatch: 'full' },
+  { path: 'anasayfa', component: HeroComponent,}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
