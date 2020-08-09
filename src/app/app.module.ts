91422857ab8cda50bdfa0db549d9799633ae32a8
Umut Arpat <arptumut@gmail.com>
2020-08-09 20:56:19 +0300

    formated hero photo,changed folder structure,changed alignment of hero containers,added social media icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { BodyComponent } from './shared/components/body/body.component';
+import { HeaderComponent } from './pages/home-page/components/header/header.component';
+import { BodyComponent } from './pages/home-page/components/body/body.component';
 import { HeroComponent } from './shared/components/hero/hero.component';
-
+import {HeaderBarComponent} from './shared/components/header-bar/header-bar.component';
+import { FooterComponent } from './pages/home-page/components/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     HeaderComponent,
     BodyComponent,
-    HeroComponent
+    HeroComponent,
+    HeaderBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
